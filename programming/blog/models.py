2fa3a49b7e46be53f89621676adb5a047851be4b
yooncobra <yooncobra@gmail.com>
2016-02-23 18:36:15 +0900

    blog.models.Post 모델 클래스의 __str__ 멤버함수 구현에 대해, 파이썬2 호환성 보장

--- programming/blog/models.py
+++ programming/blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class Post(models.Model):   # 이 클래스는 모든 모델의 기본 클래스인 models.Model을 상속 받습니다.
     title = models.CharField(max_length=100, help_text='포스팅 제목을 100자 이내로 써주세요.')
     content = models.TextField()
