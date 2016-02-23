e068e35b27ccd0fc2dac9c2e7694b96cdee00d6a
yooncobra <yooncobra@gmail.com>
2016-02-23 19:16:44 +0900

    shop.models.Comment.post 의 related_name 을 변경함으로서, related_name 충돌해결

--- programming/shop/models.py
+++ programming/shop/models.py
@@ -3,6 +3,6 @@ from blog.models import Post
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post)
+    post = models.ForeignKey(Post, related_name='shop_comment_set') # or related_name='+'
     content = models.TextField()
 
