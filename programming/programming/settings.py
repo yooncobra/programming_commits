d2f5e821e33c0b68075085f2903ba374dfed5d66
yooncobra <yooncobra@gmail.com>
2016-02-28 13:51:49 +0900

    프로젝트 공용 부모 템플릿 base.html을 생성

--- programming/programming/settings.py
+++ programming/programming/settings.py
@@ -59,7 +59,9 @@ ROOT_URLCONF = 'programming.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, "programming", "templates"),
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
