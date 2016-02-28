2da095f1a90ea2594d6d7b54e6178649057f2c86
yooncobra <yooncobra@gmail.com>
2016-02-28 15:58:31 +0900

    blog.models.Post 모델의 title 필드에 길이 제한 커스텀 validator 적용

--- programming/blog/models.py
+++ programming/blog/models.py
@@ -1,10 +1,19 @@
+from django import forms
+from django.core.validators import RegexValidator
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 
 
+def min_length_validator(value):
+    if len(value) < 3:
+        raise forms.ValidationError('3글자 이상 입력하세요!')
+
+
 @python_2_unicode_compatible
 class Post(models.Model):   # 이 클래스는 모든 모델의 기본 클래스인 models.Model을 상속 받습니다.
-    title = models.CharField(max_length=100, help_text='포스팅 제목을 100자 이내로 써주세요.')
+    title = models.CharField(max_length=100, 
+            validators=[min_length_validator],
+            help_text='포스팅 제목을 100자 이내로 써주세요.')
     content = models.TextField()
     photo = models.ImageField(blank=True)
     tags = models.ManyToManyField('Tag', blank=True)
