4d3c16151857f53f69b45265fe45ff95347dc209
yooncobra <yooncobra@gmail.com>
2016-02-29 20:14:09 +0900

    기존 SignupForm은 email 필드 처리로 ModelForm 기능을 활용했었지만, 이 번엔 SignupForm2 를 통해 이를 직접처리 하도록 함

--- programming/accounts/forms.py
+++ programming/accounts/forms.py
@@ -5,8 +5,8 @@ from django.contrib.auth  import get_user_model
 
 class SignupForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
-    # fields = ['username', 'email']
-    fields = UserCreationForm.Meta.fields + ('email',)
+        # fields = ['username', 'email']
+        fields = UserCreationForm.Meta.fields + ('email',)
 
     def clean_email(self):
         email = self.cleaned_data.get('email', None)
@@ -15,6 +15,25 @@ class SignupForm(UserCreationForm):
             if User.objects.filter(email=email).exists():
                 raise forms.ValidationError('중복된 이메일')
         return email
+
+
+class SignupForm2(UserCreationForm):
+    email = forms.EmailField()
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email', None)
+        if email:
+            User = get_user_model()
+            if User.objects.filiter(email=email).exists():
+                raise forms.ValidationError('중복된 이메일')
+        return email
+
+    def save(self, commit=True):
+        user = super(SignupForm2, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
 
 
 class LoginForm(AuthenticationForm):
