28c5368d8a5db505643b7f8d16c2aff318c48901
yooncobra <yooncobra@gmail.com>
2016-03-01 22:56:01 +0900

    blog 앱에 대한 url 처리를 blog/urls.py로 이관

--- programming/programming/urls.py
+++ programming/programming/urls.py
@@ -21,15 +21,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('accounts.urls')),
-    url(r'^$', 'blog.views.index'),
-    url(r'^posts/$', 'blog.views.post_list'),
-    url(r'^posts/(?P<pk>\d+)$', 'blog.views.post_detail'),
-
-    url(r'^posts/new/$', 'blog.views.post_new'),
-    url(r'^posts/(?P<pk>\d+)/edit/$', 'blog.views.post_edit'),
-    url(r'^posts/(?P<post_pk>\d+)/comments/new/$', 'blog.views.comment_new'),
-    url(r'^posts/(?P<post_pk>\d+)/comments/(?P<pk>\d+)/edit/$', 'blog.views.comment_edit'),
-]
+    url(r'', include('blog.urls')),
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
