b7bf1faa42510412ab8b22358438d7981576178d
yooncobra <yooncobra@gmail.com>
2016-03-06 16:52:03 +0900

    프로젝트에서 전반적으로 쓰일 base.html 템플릿을 만들고, 이를 blog/base.html로 경유해서 blog/index.html 에서 사용토록 함

--- publisher/publisher/settings/common.py
+++ publisher/publisher/settings/common.py
@@ -57,7 +57,9 @@ ROOT_URLCONF = 'publisher.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'publisher', 'templates',
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
